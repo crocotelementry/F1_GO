f42f645ef214f694274e411024cced519841302f
Kristian Nilssen <kcn0519@westminstercollege.edu>
2019-01-10 17:57:25 -0800

    Cleanup and Comments
    Cleaned up and added comments

--- hub.go
+++ hub.go
@@ -2,19 +2,6 @@ package main
 
 import (
 	"log"
-	
-	"F1_GO/structs"
-)
-
-var (
-	new_motion_packet      = make(chan structs.PacketMotionData)
-	new_session_packet     = make(chan structs.PacketSessionData)
-	new_lap_packet         = make(chan structs.PacketLapData)
-	new_event_packet       = make(chan structs.PacketEventData)
-	new_participant_packet = make(chan structs.PacketParticipantsData)
-	new_car_setup_packet   = make(chan structs.PacketCarSetupData)
-	new_telemetry_packet   = make(chan structs.PacketCarTelemetryData)
-	new_car_status_packet  = make(chan structs.PacketCarStatusData)
 )
 
 // Hub maintains the set of active clients and broadcasts messages to the
@@ -23,7 +10,7 @@ type Hub struct {
 	// Registered clients.
 	clients map[*Client]bool
 
-	// Inbound messages from F1 2018 UDP
+	// Inbound messages from F1 2018 UDP to be broacasted to connected clients
 	broadcast chan *Udp_data
 
 	// Register requests from the clients.
@@ -33,6 +20,7 @@ type Hub struct {
 	unregister chan *Client
 }
 
+// Creates a new hub
 func newHub() *Hub {
 	return &Hub{
 		broadcast:  make(chan *Udp_data),
@@ -42,51 +30,50 @@ func newHub() *Hub {
 	}
 }
 
+// Hub function that handles all connecting and disconnecting clients and the messages that are to be
+// broadcasted to them!
 func (h *Hub) run() {
 	for {
 		select {
+		// If we have a new client connecting!
 		case client := <-h.register:
 			h.clients[client] = true
 			log.Println("", client.conn.RemoteAddr(), " ", "client opened")
+
+		// If we have a client that is disconnecting
 		case client := <-h.unregister:
 			if _, ok := h.clients[client]; ok {
 				log.Println("", client.conn.RemoteAddr(), " ", "client closed")
-				delete(h.clients, client)
-				close(client.Motion_packet_send)
-				close(client.Session_packet_send)
-				close(client.Lap_packet_send)
-				close(client.Event_packet_send)
-				close(client.Participant_packet_send)
-				close(client.Car_setup_packet_send)
-				close(client.Telemetry_packet_send)
-				close(client.Car_status_packet_send)
+				delete(h.clients, client)								// Delete the client
+				close(client.Motion_packet_send)				// Close all the different packet channels!
+				close(client.Session_packet_send)				// ..
+				close(client.Lap_packet_send)						// ..
+				close(client.Event_packet_send)					// ..
+				close(client.Participant_packet_send)		// ..
+				close(client.Car_setup_packet_send)			// ..
+				close(client.Telemetry_packet_send)			// ..
+				close(client.Car_status_packet_send)		// ..
 			}
+
+		// If we have a message to broadcast
 		case message := <-h.broadcast:
-			for client := range h.clients {
-				switch message.Id {
+			for client := range h.clients {		// Loop through all our clients
+				switch message.Id {							// Depending on what packet we have to send, send that packet
 				case 0:
-					// send_message := message.Motion_packet
 					client.Motion_packet_send <- message.Motion_packet
 				case 1:
-					// send_message := message.Session_packet
 					client.Session_packet_send <- message.Session_packet
 				case 2:
-					// send_message := message.Lap_packet
 					client.Lap_packet_send <- message.Lap_packet
 				case 3:
-					// send_message := message.Event_packet
 					client.Event_packet_send <- message.Event_packet
 				case 4:
-					// send_message := message.Participant_packet
 					client.Participant_packet_send <- message.Participant_packet
 				case 5:
-					// send_message := message.Car_setup_packet
 					client.Car_setup_packet_send <- message.Car_setup_packet
 				case 6:
-					// send_message := message.Telemetry_packet
 					client.Telemetry_packet_send <- message.Telemetry_packet
 				case 7:
-					// send_message := message.Car_status_packet
 					client.Car_status_packet_send <- message.Car_status_packet
 				}
 			}
