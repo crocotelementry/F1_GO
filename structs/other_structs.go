b3b7bd5f7039f30dd2706b7c35bdb9a5aae182d8
Kristian Nilssen <kcn0519@westminstercollege.edu>
2019-02-24 22:34:18 -0800

    Update other_structs.go

--- structs/other_structs.go
+++ structs/other_structs.go
@@ -37,3 +37,28 @@ type Save_to_database_websocket_recive struct {
 
 	Uid uint64
 }
+
+type Save_to_database_status struct {
+	M_header       PacketHeader // Header
+	Status         string
+	UID            string
+	Current_packet int
+	Packet_0       int
+	Packet_0_total int
+	Packet_1       int
+	Packet_1_total int
+	Packet_2       int
+	Packet_2_total int
+	Packet_3       int
+	Packet_3_total int
+	Packet_4       int
+	Packet_4_total int
+	Packet_5       int
+	Packet_5_total int
+	Packet_6       int
+	Packet_6_total int
+	Packet_7       int
+	Packet_7_total int
+	Total_current  int
+	Total_packets  int
+}
